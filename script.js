5b97d48c3bcf6d67fe763251a0e37638b5f3720d
chrislaf63 <chrislaf63@gmail.com>
2023-10-19 11:25:46 +0200

    Modal

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const myModal = document.getElementById('exampleModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
